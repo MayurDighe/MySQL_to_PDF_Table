c034f2340074b57ed1f8fa1fa03b9b9fe3decdbc
Mayur Dighe <themayurdighe@gmail.com>
2017-06-28 01:53:53 +0530

    Add project files.

---
+++ PDF_Automation/FrmPDFAutomation.cs
@@ -0,0 +1,125 @@
+﻿//Libraries
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PDF_Automation
+{
+    public partial class FrmPDFAutomation : Form
+    {
+        public FrmPDFAutomation() { InitializeComponent(); }
+
+        //Code for "Generate PDF" button
+        private void BtnGeneratePDF_Click(object sender, EventArgs e)
+        {
+            //Check limit to retrive records
+            if (txtFrom.Text.Trim().Length == 0 && txtTo.Text.Trim().Length == 0)
+            { MessageBox.Show("Please Enter Range of Records!!"); }
+            else
+            {
+                //Variables 
+                MySqlConnection connection; //connection object to connect with MySQL database
+
+                //Database credentials
+                string server = "localhost";
+                string database = "sakila";
+                string usename = "root";
+                string password = "root";
+
+                //Connection string
+                string connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+                database + ";" + "UID=" + usename + ";" + "PASSWORD=" + password + ";";
+
+                try
+                {
+                    //Input from user - Record limits
+                    int from = Convert.ToInt32(txtFrom.Text);
+                    int to = Convert.ToInt32(txtTo.Text);
+
+                    //SQL Query to retrive data from SAKILA database (CUSTOMER table)
+                    string query = "SELECT customer_id, first_name, last_name, email FROM customer where customer_id between " + from + " and " + to + "";
+
+                    //Create a list to store the result
+                    List<string>[] list = new List<string>[4];
+                    list[0] = new List<string>();
+                    list[1] = new List<string>();
+                    list[2] = new List<string>();
+                    list[3] = new List<string>();
+                    
+                    //Variables to create PDF document
+                    Document doc = new Document();
+                    PdfWriter.GetInstance(doc, new FileStream(@"MySQL_to_PDF_table.pdf", FileMode.Create)); //Output PDF file
+                    PdfPTable table = new PdfPTable(4); //To create table inside the PDF
+
+                    //Width of each column on table
+                    float[] widths = { 70f, 110f, 110f, 300f };
+                    table.SetWidthPercentage(widths, PageSize.A4);
+
+                    //Font settings
+                    Font titleFont = FontFactory.GetFont("Verdana");
+                    titleFont.SetStyle("bold");
+
+                    //Connect with MySQL database
+                    using (connection = new MySqlConnection(connectionString))
+                    {
+                        connection.Open(); //Access SAKILA database
+
+                        //Prepare SQL query to retrive data
+                        MySqlCommand cmd = new MySqlCommand(query, connection);
+                        MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                        doc.Open(); //Access the PDF Document to write data
+
+                        //Add table column headers
+                        table.AddCell(new Paragraph("Record ID", titleFont));
+                        table.AddCell(new Paragraph("First Name", titleFont));
+                        table.AddCell(new Paragraph("Last Name", titleFont));
+                        table.AddCell(new Paragraph("Email ID", titleFont));
+
+                        //Read the data and store them in the list
+                        while (dataReader.Read())
+                        {
+                            //read data using DataReader
+                            string value1 = dataReader["customer_id"].ToString();
+                            string value2 = dataReader["first_name"].ToString();
+                            string value3 = dataReader["last_name"].ToString();
+                            string value4 = dataReader["email"].ToString();
+
+                            //Add data to table
+                            table.AddCell(value1);
+                            table.AddCell(value2);
+                            table.AddCell(value3);
+                            table.AddCell(value4);
+                        }
+
+                        //Close the DataReader
+                        dataReader.Close();
+
+                        //Close database connection
+                        connection.Close();
+                    }
+
+                    //Add data to PDF file
+                    doc.Add(new Paragraph("MySQL Database to PDF Table using iText Library\n\n", titleFont));
+                    doc.Add(table);
+                    doc.Add(new Paragraph("\nCopyright 2017 @ http://howtocodefor.website", titleFont));
+
+                    //Close PDF document
+                    doc.Close();
+
+                    //Open the Output file
+                    Process.Start(@"MySQL_to_PDF_table.pdf");
+
+                }
+                catch (Exception ex) { MessageBox.Show(ex.ToString()); }
+            }
+        }
+    }
+}
+
+
---
+++ PDF_Automation/frmPDFAutomation.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace PDF_Automation
+{
+    partial class FrmPDFAutomation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGeneratePDF = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.linkCopyright = new System.Windows.Forms.LinkLabel();
+            this.gbSelectRecords = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.lblNote = new System.Windows.Forms.Label();
+            this.gbSelectRecords.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGeneratePDF
+            // 
+            this.btnGeneratePDF.Font = new System.Drawing.Font("Verdana", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGeneratePDF.Location = new System.Drawing.Point(389, 102);
+            this.btnGeneratePDF.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGeneratePDF.Name = "btnGeneratePDF";
+            this.btnGeneratePDF.Size = new System.Drawing.Size(293, 108);
+            this.btnGeneratePDF.TabIndex = 0;
+            this.btnGeneratePDF.Text = "Generate PDF";
+            this.btnGeneratePDF.UseVisualStyleBackColor = true;
+            this.btnGeneratePDF.Click += new System.EventHandler(this.BtnGeneratePDF_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(724, 59);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "MySQL Database to PDF Table using iText Library";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // linkCopyright
+            // 
+            this.linkCopyright.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.linkCopyright.LinkArea = new System.Windows.Forms.LinkArea(17, 44);
+            this.linkCopyright.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.linkCopyright.Location = new System.Drawing.Point(0, 277);
+            this.linkCopyright.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkCopyright.Name = "linkCopyright";
+            this.linkCopyright.Size = new System.Drawing.Size(724, 55);
+            this.linkCopyright.TabIndex = 2;
+            this.linkCopyright.TabStop = true;
+            this.linkCopyright.Text = "Copyright 2017 @ http://howtocodefor.website";
+            this.linkCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkCopyright.UseCompatibleTextRendering = true;
+            // 
+            // gbSelectRecords
+            // 
+            this.gbSelectRecords.Controls.Add(this.label4);
+            this.gbSelectRecords.Controls.Add(this.label3);
+            this.gbSelectRecords.Controls.Add(this.txtTo);
+            this.gbSelectRecords.Controls.Add(this.txtFrom);
+            this.gbSelectRecords.Location = new System.Drawing.Point(34, 92);
+            this.gbSelectRecords.Name = "gbSelectRecords";
+            this.gbSelectRecords.Size = new System.Drawing.Size(262, 118);
+            this.gbSelectRecords.TabIndex = 8;
+            this.gbSelectRecords.TabStop = false;
+            this.gbSelectRecords.Text = "Select Record IDs";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(178, 37);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "To";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 37);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 23);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "From";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(155, 64);
+            this.txtTo.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(66, 29);
+            this.txtTo.TabIndex = 9;
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(40, 64);
+            this.txtFrom.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(66, 29);
+            this.txtFrom.TabIndex = 8;
+            // 
+            // lblNote
+            // 
+            this.lblNote.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblNote.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNote.Location = new System.Drawing.Point(0, 234);
+            this.lblNote.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNote.Name = "lblNote";
+            this.lblNote.Size = new System.Drawing.Size(724, 43);
+            this.lblNote.TabIndex = 11;
+            this.lblNote.Text = "Note : The database used here for demonstration is MySQL\'s sakila database.";
+            this.lblNote.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmPDFAutomation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 332);
+            this.Controls.Add(this.lblNote);
+            this.Controls.Add(this.gbSelectRecords);
+            this.Controls.Add(this.linkCopyright);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnGeneratePDF);
+            this.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmPDFAutomation";
+            this.Text = "PDF Automation";
+            this.gbSelectRecords.ResumeLayout(false);
+            this.gbSelectRecords.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGeneratePDF;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.LinkLabel linkCopyright;
+        private System.Windows.Forms.GroupBox gbSelectRecords;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.Label lblNote;
+    }
+}
+
